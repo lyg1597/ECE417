6fa66efa069d2cecbdf3d33f8196efbcd2df048a
Li <li213@illinois.edu>
2017-09-09 15:40:48 -0500

    ec probably done

--- mp1/finalRelease/run.m
+++ mp1/finalRelease/run.m
@@ -1,21 +1,30 @@
 function run
 %RUN function to run the mp1 program and plot the result
 %   Detailed explanation goes here
-
-cbirMP;
-pvec = [];
-p = [];
-done = false;
-while ~done
-    pvec = [pvec p];    
-    done = input('Enter 1 to exit, 0 to continue: ');
-    done = logical(done);
+for n=1:5
+    disp(' ');
+    text=['session: ',num2str(n)];
+    disp(text);
+    cbirMP;
+    pvec = [];
+    done = false;
+    round=0;    
+    while round<=3
+       text=['round: ',num2str(round)];
+        disp(text);
+        done = input('Press any key to continue after pressing query: ');
+        round=round+1;
+        pvec = [pvec,evalin('base','p')];  
+    end
+    close;
+    subplot(5,1,n);
+    plot(pvec(2:4));
+    text=['mp1 session ',num2str(n)];
+    title(text);
+    xlim([1 3]); 
+    ylim([0 1]); 
+    grid on;  
 end
-close all;
-plot(pvec);
-xlim([1 3]); 
-ylim([0 1]); 
-grid on;  
 
 end
 
